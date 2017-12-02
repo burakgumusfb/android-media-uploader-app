ba1b47d44ae3159fa35b30331467874252e8d5c4
DESKTOP-VJ937TR\BURAK <burakgumusfb@gmail.com>
2017-12-02 23:34:50 +0300

    project upload.

---
+++ app/src/main/java/com/example/mediauploader/androidmediauploader/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.mediauploader.androidmediauploader;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
